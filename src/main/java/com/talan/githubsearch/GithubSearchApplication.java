1806a24ca272ada037d5f6888c3d48b6a347a26a
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-08-03 13:17:48 +0200

    Setup project frame

---
+++ src/main/java/com/talan/githubsearch/GithubSearchApplication.java
@@ -0,0 +1,13 @@
+package com.talan.githubsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubSearchApplication.class, args);
+    }
+
+}
