b6e5c1f994b8b1057ae751ee95331bb9754526f5
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-08-07 14:47:23 +0200

    Style search bar

--- src/ui/src/app/nav-bar/components/navbar/navbar.component.ts
+++ src/ui/src/app/nav-bar/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {FormControl} from "@angular/forms";
 import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar', templateUrl: './navbar.component.html', styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  githubUserControl = new FormControl('', [Validators.required]);
+  githubUserControl = new FormControl('', []);
 
 
   constructor(private readonly router: Router) {
