dfdadc08a251646763ecfe6390fc6b9f53143ddb
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-08-07 11:16:05 +0200

    Change cors to allow only localhost access

--- src/ui/src/app/app-routing.module.ts
+++ src/ui/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomePageComponent} from "./components/home-page/home-page.component";
 import {AppComponent} from "./app.component";
 import {GithubUserComponent} from "./github-user/components/github-user/github-user.component";
 
