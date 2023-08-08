06b26fb77069b5b27bc927d592a84b49968c23de
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-08-08 17:20:32 +0200

    Simplify ResourceHandlerConfig

--- src/main/java/com/talan/githubsearch/config/ResourceHandlerConfig.java
+++ src/main/java/com/talan/githubsearch/config/ResourceHandlerConfig.java
@@ -10,22 +10,23 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 import java.io.IOException;
 
 @Configuration
-class WebConfig implements WebMvcConfigurer {
+class ResourceHandlerConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath,
-                                                   Resource location) throws IOException {
-                        Resource requestedResource = location.createRelative(resourcePath);
-                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/static/index.html");
-                    }
-                });
+                .addResolver(new CustomPathResourceResolver());
+    }
+
+    private static class CustomPathResourceResolver extends PathResourceResolver {
+        @Override
+        protected Resource getResource(String resourcePath, Resource location) throws IOException {
+            Resource requestedResource = location.createRelative(resourcePath);
+            return requestedResource.exists() && requestedResource.isReadable()
+                    ? requestedResource
+                    : new ClassPathResource("/static/index.html");
+        }
     }
 }
