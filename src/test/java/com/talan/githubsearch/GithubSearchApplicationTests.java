deba2cb1de30772c9d947fa6348b65ae514d5a28
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-08-09 14:10:07 +0200

    Add remote database

--- src/test/java/com/talan/githubsearch/GithubSearchApplicationTests.java
+++ src/test/java/com/talan/githubsearch/GithubSearchApplicationTests.java
@@ -1,13 +1,13 @@
 package com.talan.githubsearch;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GithubSearchApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
